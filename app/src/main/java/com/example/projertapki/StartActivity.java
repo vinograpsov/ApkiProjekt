182f1349846ebfa51aeaed5a8a93cfc79c191aa3
vinograpsov <kirvinpost@gmail.com>
2022-05-22 14:07:35 +0300

    starts with StartActuvity

--- app/src/main/java/com/example/projertapki/StartActivity.java
+++ app/src/main/java/com/example/projertapki/StartActivity.java
@@ -2,13 +2,41 @@ package com.example.projertapki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    private Button  register, login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        register = findViewById(R.id.registration);
+        login = findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
