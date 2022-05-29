d9e186072538b310222419089fc64f1b299d2aaa
vinograpsov <kirvinpost@gmail.com>
2022-05-29 13:40:00 +0300

    made registration

--- app/src/main/java/com/example/projertapki/RegistrationActivity.java
+++ app/src/main/java/com/example/projertapki/RegistrationActivity.java
@@ -1,14 +1,71 @@
 package com.example.projertapki;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private EditText email, password;
+    private Button register;
+    private TextView warning;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        email = findViewById(R.id.emailReg);
+        password = findViewById(R.id.passwordReg);
+        register = findViewById(R.id.registrationAcc);
+        warning = findViewById(R.id.textView6);
+
+        auth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txtEmail = email.getText().toString();
+                String txtPassword = password.getText().toString();
+
+                if (TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)) {
+                    Toast.makeText(RegistrationActivity.this, "Empty email of password pole", Toast.LENGTH_SHORT).show();
+                } else if (txtPassword.length() < 6) {
+                    Toast.makeText(RegistrationActivity.this, "Password to short", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(txtEmail, txtPassword);
+                }
+            }
+        });
+    }
+    private void registerUser(String email, String password){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    warning.setText("Succsessful");
+                    warning.setTextColor(Color.GREEN);
+                }
+                else{
+                    warning.setText("FAILED");
+                    warning.setTextColor(Color.RED);
+                }
+            }
+        });
+
     }
 }
