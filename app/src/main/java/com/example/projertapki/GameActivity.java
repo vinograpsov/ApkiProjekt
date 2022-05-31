eeaa87365535408153c971fd8361222943a29dce
vinograpsov <kirvinpost@gmail.com>
2022-05-31 21:16:44 +0300

    before chage game xml

--- app/src/main/java/com/example/projertapki/GameActivity.java
+++ app/src/main/java/com/example/projertapki/GameActivity.java
@@ -3,6 +3,7 @@ package com.example.projertapki;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,11 +11,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.ktx.Firebase;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
     private Button answer1, answer2, answer3 , answer4;
     private FirebaseAuth auth;
+    List<Button> buttons = new ArrayList<>();
 //    private TextView lifes, timer;
 //    private int lifesInt = 3;
 
@@ -27,21 +31,52 @@ public class GameActivity extends AppCompatActivity {
 //        timer = findViewById(R.id.timer);
 
 //        lifes.setText(lifesInt);
-        Button[] buttons = new Button[4];
-        buttons[0] = answer1;
-        buttons[1] = answer2;
-        buttons[2] = answer3;
-        buttons[3] = answer4;
-        randomPos(buttons);
+//        Button[] buttons = new Button[4];
+//        buttons[0] = answer1;
+//        buttons[1] = answer2;
+//        buttons[2] = answer3;
+//        buttons[3] = answer4;
+
+
+        answer1 = findViewById(R.id.button1);
+        answer1.setText("1");
+
+        answer2 = findViewById(R.id.button2);
+        answer2.setText("2");
+
+        answer3 = findViewById(R.id.button3);
+        answer3.setText("3");
+
+        answer4 = findViewById(R.id.button4);
+        answer4.setText("4");
+
+
+
+        buttons.add(answer1);
+        buttons.add(answer2);
+        buttons.add(answer3);
+        buttons.add(answer4);
+
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                randomPos2(buttons);
+            }
+        });
+
+//        randomPos1(buttons);
+//        timer.setText(answer3.getText());
 
 
    }
 
 
-   private List<Integer> randomButtons(){
+
+
+   private List<Integer> randomButtonsNum(){
         List<Integer> randoms = new ArrayList<>();
         while (randoms.size()!= 4){
-            int randomNum = 1 + (int)(Math.random() * ((5 - 1) + 1));
+            int randomNum = (int)(Math.random() * 4);
             if (!randoms.contains(randomNum)){
                 randoms.add(randomNum);
             }
@@ -49,8 +84,37 @@ public class GameActivity extends AppCompatActivity {
         return randoms;
    }
 
+    public void randomPos2(List<Button> buttons){
+        Collections.shuffle(buttons);
+    }
+
+
+
+    public void randomPos1(Button[] buttons){
+       List<Integer> randoms = randomButtonsNum();
+       buttons[randoms.get(0)] = findViewById(R.id.button1);
+       buttons[randoms.get(0)].setText("1");
+       buttons[randoms.get(0)].setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               randomPos1(buttons);
+               System.out.println("click");
+               System.out.println(randoms);
+           }
+       });
+       buttons[randoms.get(1)] = findViewById(R.id.button2);
+       buttons[randoms.get(1)].setText("2");
+
+       buttons[randoms.get(2)] = findViewById(R.id.button3);
+       buttons[randoms.get(2)].setText("3");
+
+       buttons[randoms.get(3)] = findViewById(R.id.button4);
+       buttons[randoms.get(3)].setText("4");
+   }
+
+
    private void randomPos(Button[] buttons){
-       List<Integer> randoms = randomButtons();
+       List<Integer> randoms = randomButtonsNum();
        for (int i  = 0 ; i < randoms.size(); i++){
            int temp = randoms.get(i);
            switch (temp){
