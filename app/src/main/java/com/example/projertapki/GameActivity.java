441dcdc7939519fdaf742d78bd2afb92dd9f95ec
vinograpsov <kirvinpost@gmail.com>
2022-06-04 17:01:16 +0300

    all good but points dont add gue to different times

--- app/src/main/java/com/example/projertapki/GameActivity.java
+++ app/src/main/java/com/example/projertapki/GameActivity.java
@@ -1,59 +1,74 @@
 package com.example.projertapki;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.ktx.Firebase;
-
-import java.sql.Time;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class GameActivity extends AppCompatActivity{
     private Button answer1, answer2, answer3 , answer4;
-    private FirebaseAuth auth;
-    TextView lifesTextView;
-    TextView timerView;
-    private int lifes;
     ArrayList<String> answers = new ArrayList<>();
-    public int timeCounter;
+    TextView lifesTextView,timerView,pointsView;
+    private CountDownTimer myTimer;
 
+    private int lifes,points,rightAnswer;
+
+
+    private FirebaseAuth auth;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        lifes = 3;
-        lifesTextView  = findViewById(R.id.lifes);
+
         timerView = findViewById(R.id.timer);
-        lifesTextView.setText(Integer.toString(lifes));
+        lifesTextView  = findViewById(R.id.lifes);
+        pointsView = findViewById(R.id.points);
         answer1 = findViewById(R.id.button1);
         answer2 = findViewById(R.id.button2);
         answer3 = findViewById(R.id.button3);
         answer4 = findViewById(R.id.button4);
+
+
+        points = 0;
+        lifes = 3;
+        rightAnswer = 3;
+
+        pointsView.setText(Integer.toString(points));
+        lifesTextView.setText(Integer.toString(lifes));
+
+
         answers.add("1");
         answers.add("2");
         answers.add("3");
         answers.add("4");
 
-        setAnsweersOnButtons(answer1,answer2,answer3,answer4,answers);
+        setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
 
-        startTimer();
+
+
+        myTimer =  new CountDownTimer(11000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timerView.setText("" + TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished));
+            }
+            public void onFinish() {
+                lifes -= 1;
+                myTimer.cancel();
+                myTimer.start();
+                lifesTextView.setText(Integer.toString(lifes));
+                if(lifes <= 0){
+                    timerView.setText(":(");
+                }
+            }
+        }.start();
+
+
+        System.out.println("3" == Integer.toString(rightAnswer));
 
 
     }
@@ -63,7 +78,7 @@ public class GameActivity extends AppCompatActivity{
         return buttonNums;
     }
 
-    private void setAnsweersOnButtons(Button answer1,Button answer2,Button answer3,Button answer4,ArrayList<String> answers){
+    private void setAnswersOnButtons(Button answer1, Button answer2, Button answer3, Button answer4, ArrayList<String> answers){
         randomButtonPos(answers);
         answer1.setText(answers.get(0));
         answer2.setText(answers.get(1));
@@ -72,63 +87,71 @@ public class GameActivity extends AppCompatActivity{
     }
 
     public void shuffleButtons1(View view){
-        startTimer();
-        if(answer1.getText() == "1"){
-            setAnsweersOnButtons(answer1,answer2,answer3,answer4,answers);
-            System.out.println("clicked 1 ");
+//        if(answer1.getText() == Integer.toString(rightAnswer)){
+        if(answer1.getText() == "" + rightAnswer){
+            points += 1;
+            myTimer.cancel();
+            myTimer.start();
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
+            pointsView.setText(Integer.toString(points));
+        }
+        else{
+            myTimer.cancel();
+            myTimer.start();
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
             lifes -= 1;
             lifesTextView.setText(Integer.toString(lifes));
         }
     }
 
     public void shuffleButtons2(View view){
-        startTimer();
-        if(answer2.getText() == "1"){
-            setAnsweersOnButtons(answer1,answer2,answer3,answer4,answers);
-            System.out.println("clicked 2 ");
+        if(answer2.getText() == "" + rightAnswer){
+            myTimer.cancel();
+            myTimer.start();
+            points +=1;
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
+            pointsView.setText(Integer.toString(points));
+        }
+        else{
+            myTimer.cancel();
+            myTimer.start();
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
             lifes -= 1;
             lifesTextView.setText(Integer.toString(lifes));
         }
     }
 
     public void shuffleButtons3(View view){
-        startTimer();
-        if(answer3.getText() == "1"){
-            setAnsweersOnButtons(answer1,answer2,answer3,answer4,answers);
-            System.out.println("clicked 3 ");
+        if(answer3.getText() == "" + rightAnswer){
+            myTimer.cancel();
+            myTimer.start();
+            points +=1;
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
+            pointsView.setText(Integer.toString(points));
+        }
+        else{
+            myTimer.cancel();
+            myTimer.start();
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
             lifes -= 1;
             lifesTextView.setText(Integer.toString(lifes));
         }
     }
 
     public void shuffleButtons4(View view){
-        startTimer();
-        if(answer4.getText() == "1"){
-            setAnsweersOnButtons(answer1,answer2,answer3,answer4,answers);
-            System.out.println("clicked 4 ");
+        if(answer4.getText() == "" + rightAnswer){
+            myTimer.cancel();
+            myTimer.start();
+            points +=1;
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
+            pointsView.setText(Integer.toString(points));
+        }
+        else{
+            myTimer.cancel();
+            myTimer.start();
+            setAnswersOnButtons(answer1,answer2,answer3,answer4,answers);
             lifes -= 1;
             lifesTextView.setText(Integer.toString(lifes));
         }
     }
-
-    public void startTimer(){
-        new CountDownTimer(10000, 1000) { // adjust the milli seconds here
-
-            public void onTick(long millisUntilFinished) {
-                timerView.setText("" + TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished));
-            }
-            public void onFinish() {
-                lifes -= 1;
-                if(lifes > 0){
-                    startTimer();
-                }
-                else{
-                    timerView.setText(":(");
-                }
-
-            }
-        }.start();
-    }
-
-
 }
